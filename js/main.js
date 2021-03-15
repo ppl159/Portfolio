bfa1ef5c42faac546380df22cc714209f9d1aed6
ppl159 <79615449+ppl159@users.noreply.github.com>
2021-03-15 09:43:10 +0900

    0315

--- js/main.js
+++ js/main.js
@@ -1,10 +1,56 @@
 jQuery(document).ready(function(){
+    //메뉴 버튼 클릭하면 주메뉴(nav) 나옴
+    $('.menu_btn').click(function(){
+        //nav영역이 보여진 후에 function다음의 명령어 실행(메뉴 페이드인)
+       $('nav').show(function(){
+           $('nav ul li').each(function(){
+              var indexNum=$(this).index();
+               $(this).delay(indexNum*300).fadeIn();
+           });
+       }); 
+    });
+    //메뉴의 닫기 버튼을 클릭하면 주메뉴 안보임
+    $('nav .close').click(function(){
+       $('nav').hide();                   
+    });
+    
+    //주메뉴를 클릭하면 해당 section으로 이동
+    $('nav ul li a').click(function(){
+       $('nav').hide();
+        var menu_attr=$(this).attr('href');
+        $('html,body').animate({
+           scrollTop:$(menu_attr).offset().top},1000);
+        });
+    
+    
+    
     //스킬바
-	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},3000);
+    $(window).scroll(function(){
+       //화면 맨위쪽 위치:0을 변수에 저장
+        var winTop=$(this).scrollTop();
+        //화면 아래쪽 위치를 변수에 저장
+        var winBottom=winTop+$(window).height();
+        //두번째 section의 시작 위치값을 변수에 저장
+        var secTop=$('.profile').offset().top;
+        
+        if(secTop<winTop && secTop>winBottom){
+            
+        $('.skillbar').each(function(){
+		$(this).find('.skillbar-bar').animate({
+        width:$(this).attr('data-percent')
+		},1000);
+	});  
+        }else{
+        $('.skillbar').each(function(){
+		$(this).find('.skillbar-bar').animate({
+        width:0
+		},1000);
 	});
+        }
+    });
+    
+    
+    
     
     //swiper 슬라이드(web)
     var swiper = new Swiper('.swiper-container', {
